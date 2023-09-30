7d04e0541c71b29713c7cd91c133d4107b057bc1
Kai <kau.matosr@gmail.com>
2023-09-30 20:44:35 -0300

    Style: removing unnecessary logs

---
+++ src/app/hooks/useApi.tsx
@@ -0,0 +1,57 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import { useUser } from './useUser'
+import env from 'src/constants/env'
+import { useSnackbar } from './useSnackbar'
+
+const useApi = () => {
+    const { user } = useUser()
+    const { createSnack } = useSnackbar()
+
+    const authConfig: AxiosRequestConfig = {
+        headers: {
+            'Authorization': 'Bearer ' + user?.accessToken
+        }
+    }
+
+    const successCallback = (response: AxiosResponse<any, any>) => (
+        response.data
+    )
+    
+    const errorCallback = (error: any) => {
+        console.error(error)
+        createSnack('Um erro inesperado ocorreu, tente novamente', 'error')
+    }
+
+    const get = (url: string, useAuth = true) => {
+        return axios.get(`${env.dataApiUrl}${url}`, useAuth ? authConfig : undefined)
+            .then(successCallback)
+            .catch(errorCallback)
+    }
+
+    const post = (url: string, payload: any, useAuth = true) => {
+        return axios.post(`${env.dataApiUrl}${url}`, payload, useAuth ? authConfig : undefined)
+            .then(successCallback)
+            .catch(errorCallback)
+    }
+
+    const put = (url: string, payload: any, useAuth = true) => {
+        return axios.put(`${env.dataApiUrl}${url}`, payload, useAuth ? authConfig : undefined)
+            .then(successCallback)
+            .catch(errorCallback)
+    }
+
+    const del = (url: string, useAuth = true) => {
+        return axios.delete(`${env.dataApiUrl}${url}`, useAuth ? authConfig : undefined)
+            .then(successCallback)
+            .catch(errorCallback)
+    }
+
+    return {
+        get,
+        post,
+        put,
+        del
+    }
+}
+
+export default useApi
