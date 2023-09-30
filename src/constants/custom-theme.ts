72c0294ac0639395bac16b043af73199ef8d74d9
Kai <kau.matosr@gmail.com>
2023-09-30 19:44:13 -0300

    Fix: theme colors correction

--- src/constants/custom-theme.ts
+++ src/constants/custom-theme.ts
@@ -4,16 +4,38 @@ export const customTheme: Theme = createTheme({
     palette: {
         mode: 'light',
         primary: {
-            main: '#8f09a7',
-            light: '#c78bd3',
-            dark: '#3a0086',
-            contrastText: '#000'
+            50: '#e7e0f0',
+            100: '#c4b3db',
+            200: '#9d80c3',
+            300: '#754daa',
+            400: '#582698',
+            500: '#3a0086',
+            600: '#34007e',
+            700: '#2c0073',
+            800: '#250069',
+            900: '#180056',
+            A100: '#9e88ff',
+            A200: '#7555ff',
+            A400: '#4b22ff',
+            A700: '#3608ff',
+            'contrastText': 'white'
         },
         secondary: {
-            main: '#86bd22',
-            light: '#c3dd97',
-            dark: '#236500',
-            contrastText: '#000'
+            50: '#f0f7e4',
+            100: '#dbebbd',
+            200: '#c3de91',
+            300: '#aad164',
+            400: '#98c743',
+            500: '#86bd22',
+            600: '#7eb71e',
+            700: '#73ae19',
+            800: '#69a614',
+            900: '#56980c',
+            A100: '#e2ffc7',
+            A200: '#c8ff94',
+            A400: '#aeff61',
+            A700: '#a1ff47',
+            'contrastText': 'black'
         }
     }
 })
