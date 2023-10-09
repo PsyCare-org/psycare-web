2fcaef707a2188d07bf8a6ad2f31c14780633aa6
Kai <kau.matosr@gmail.com>
2023-10-09 15:51:05 -0300

    Feat: breadcrumb scroll shadow

--- src/app/components/molecules/breadcrumb/breadcrumb.tsx
+++ src/app/components/molecules/breadcrumb/breadcrumb.tsx
@@ -2,6 +2,7 @@ import { Breadcrumbs, Typography } from '@mui/material'
 import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import { useNavigate } from 'react-router-dom'
 import './breadcrumb.scss'
+import { useEffect, useState } from 'react'
 
 export type BreadcrumbItem = {
     url: string
@@ -17,11 +18,20 @@ export const MolBreadcrumb = ({ items }: Props) => {
 
     const navigate = useNavigate()
 
+    const [isPageScrolled, setIsPageScrolled] = useState<boolean>(false)
+
+    useEffect(() => {
+        document.querySelector('#default-template > main')?.addEventListener('scroll', evt => {
+            setIsPageScrolled((evt.target as HTMLElement).scrollTop > 0)
+        })
+    }, [])
+
     return (
         <Breadcrumbs 
             id='mol-breadcrumb'
             separator={<NavigateNextIcon fontSize="small" />}
             sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            { ...isPageScrolled && { className: 'page-scrolled' } }
         >
             { items && items.map((item, index) => (
                 <Typography 
