6ac657eebdded57e13a3f33ab625f408893aee50
Kai <kau.matosr@gmail.com>
2023-10-09 14:59:05 -0300

    Feat: highlighting current route

--- src/app/components/atoms/header-pages/header-pages.tsx
+++ src/app/components/atoms/header-pages/header-pages.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react'
 import { useUser } from 'src/app/hooks'
 import { Page, professionalPages, userPages } from './utils/pages'
 import { Button, List } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import './header-pages.scss'
 
 export const HeaderPages = () => {
 
+    const { pathname } = useLocation()
     const navigate = useNavigate()
 
     const { user } = useUser()
@@ -25,6 +26,7 @@ export const HeaderPages = () => {
                 <Button
                     key={page.url}
                     variant='text'
+                    className={pathname === page.url ? 'active' : ''}
                     sx={{ color: (theme) => theme.palette.grey[700] }}
                     onClick={() => navigate(page.url)}
                 >
